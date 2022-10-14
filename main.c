58eceda29a2ec2d8feaa8cb5fe11cb675cff4313
Unknow002 <112846383+Unknow002@users.noreply.github.com>
2022-10-14 21:14:28 +0700

    main
    the main

---
+++ main.c
@@ -0,0 +1,54 @@
+int colds, rows;
+int scl = 20;
+int w = 2600;
+int h = 1500;
+
+float[] [] terrain;
+float flying = 0;
+
+  void setup() {
+    size (1600, 12--, P3D);
+    
+
+    cols = w / scl;
+    rows = h / scl;
+    
+    terrain = new float[cols][rows];
+  
+    }
+   y += 0.05;
+}
+      
+}
+
+void draw() {
+   flying -= 0.05
+  float yoff = flying;
+    for(int y = 0; y < rows; y++) {
+      float xoff = 0;
+        for(int x = 0; x < cols; x++) {
+          terrain[x][y[ = map(noise(xoff,yoff), 0, 1 ,-100 , -150, 150;
+          xoff += 0.1;
+        }
+        yoff += 0.1
+}
+  
+    background(0);
+    stroke (255);
+    nofill();
+
+    translate(width/2, height/2);
+    rotatateX(PI/3);
+
+    translate(-w/2, -h/2);
+
+    for(int y = 0; y < rows - 1; y++) {
+        beginShape(TRIANGLE_STRIP);
+    for(int x = 0; x < cols; x++) {
+            vertex(x * scl, y * scl, terrain[x][y];
+            vertex(x * scl, (y+1) * scl, terrain[x][y+1]);
+            //rect(x * scl, y * scl, scl, scl);
+        }
+        endShape();
+    }
+}
